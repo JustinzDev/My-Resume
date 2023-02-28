6e11996ecef094ad1b63527cff2b60319c91f4e9
JustinzDev <fullyz1532@gmail.com>
2023-02-28 13:48:20 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,13 @@ import LeftMenu from './components/pages/left-menu'
 import Resume from './components/pages/resume'
 
 const App: FunctionComponent = () => {
-    const [getSlide, setSlide] = useState(false)
-
-    const SlideLeftMenu = (status: boolean) => {
-        setSlide(status)
-    }
-
     return (
         <>
-            <div className={`fixed hidden 2xl:block ${getSlide ? 'left-[260px] origin-center -rotate-180' : '2xl:left-[450px] xl:left-[330px] lg:left-[230px]' } top-[20px] z-[1] duration-500 cursor-pointer`} onClick={() => SlideLeftMenu(getSlide ? false : true)}>
-                <FontAwesomeIcon className='text-gray-300 text-[20px]' icon={faAngleLeft} />
-            </div>
             <div className='flex w-full h-screen'>
-                <div className={`${getSlide ? 'w-[15%]' : 'w-[25%]' } duration-500`}>
+                <div className={`2xl:w-[15%] w-[30%] duration-500`}>
                     <LeftMenu />
                 </div>
-                <div className={`${getSlide ? 'w-[85%]' : 'w-[75%]' } duration-500`}>   
+                <div className={`2xl:w-[85%] w-[70%] duration-500`}>   
                     <Routes>
                         <Route path="/" element={<Resume />} />
                     </Routes>
